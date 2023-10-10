eb85436cc98ed8575e1f7bbfb415563b4be31784
nachoseve <141686969+nachoseve@users.noreply.github.com>
2023-10-10 19:30:55 -0300

    add codes

--- src/Data/SiniestroData.java
+++ src/Data/SiniestroData.java
@@ -14,6 +14,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 /**
@@ -44,7 +46,7 @@ public class SiniestroData {
            
           ResultSet rs = ps.getGeneratedKeys();
           if(rs.next()){
-              siniestro.setIdSiniestro(rs.getInt("codigo"));
+              siniestro.setIdSiniestro(rs.getInt("idSiniestro"));
           }else{
               System.out.println("No se pudo obtener ID");
         }
@@ -52,4 +54,122 @@ public class SiniestroData {
             System.out.println(ex.getMessage());
     }
  }
+         
+         public Siniestro buscarSiniestro(int id){
+        Siniestro siniestro = null; 
+        String sql = "SELECT idSiniestro, tipo, fecha_siniestro, coord_X, coord_Y, detalles, fecha_resol, puntuacion, codBrigada FROM siniestro WHERE idSiniestro = ?";
+        PreparedStatement ps = null;
+        try{
+            ps = con.prepareStatement(sql);
+            ps.setInt(1,id);
+            
+            ResultSet rs = ps.executeQuery();
+            
+            if(rs.next()){
+            siniestro = new Siniestro();
+            ;
+            bombero.setNombrecompleto(rs.getString("nombre_ape"));
+            bombero.setFechaNac(rs.getDate("fecha_nac").toLocalDate());
+            bombero.setCelular(rs.getString("celular"));
+        //    bombero.setBrigada(rs.getObject("codBrigada", type)); //brigada
+        JOptionPane.showMessageDialog(null, bombero.getNombrecompleto());
+            
+        }else {
+                JOptionPane.showMessageDialog(null, "No existe el bombero");
+                }
+        ps.close();
+        } catch (SQLException ex){
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla bombero "+ex.getMessage());
+        }
+        return bombero;
+    }
+    public Bombero buscarBomberoPorDni(int dni){
+        Bombero bombero = null;
+        String sql = "SELECT id_bombero, dni, nombre_ape, fecha_nac, celular, codBrigada FROM bombero WHERE dni = ?";
+        PreparedStatement ps = null;
+        try{
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, dni);
+            ResultSet rs = ps.executeQuery();
+            
+            if(rs.next()){
+               bombero = new Bombero();
+               bombero.setIdBombero(rs.getInt("id_bombero"));
+               bombero.setDni(rs.getString("dni"));
+               bombero.setNombrecompleto(rs.getString("nombre_ape"));
+               bombero.setFechaNac(rs.getDate("fecha_nac").toLocalDate());
+               bombero.setCelular(rs.getString("celular"));
+        //       bombero.setBrigada(rs.getObject("codBrigada", type));//brigada
+               
+            }else{
+                JOptionPane.showMessageDialog(null, "No existe bombero con ese id");
+            }
+            
+            ps.close();
+        }catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla bombero "+ex.getMessage());
+        }
+        return bombero;
+    }
+    public List<Bombero> listarBomberos(){
+        List<Bombero> alumnos = new ArrayList<>();
+        try{
+            String sql = "SELECT * FROM bombero where 1";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            
+            while(rs.next()){
+               Bombero bombero = new Bombero();
+               bombero.setIdBombero(rs.getInt("id_bombero"));
+               bombero.setDni(rs.getString("dni"));
+               bombero.setNombrecompleto(rs.getString("nombre_ape"));
+               bombero.setFechaNac(rs.getDate("fecha_nac").toLocalDate());
+               bombero.setCelular(rs.getString("celular"));
+        //       bombero.setBrigada(rs.getObject("codBrigada", type));//brigada
+                      JOptionPane.showMessageDialog(null, bombero.getNombrecompleto());        
+            }
+            ps.close();
+        } catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Alumno "+ex.getMessage());
+        }
+        return alumnos;
+    }
+    public void modificaBombero(Bombero bombero){
+        String sql = "UPDATE bombero SET dni = ? , nombre_ape = ?, fecha_nac= ?, grupoSanguineo = ?, celular= ?, codBrigada= ? WHERE id_bombero =?";
+        PreparedStatement ps = null;
+        try{
+            ps = con.prepareStatement(sql);
+            ps.setString(1, bombero.getDni());
+            ps.setString(2, bombero.getNombrecompleto());
+            ps.setDate(3, Date.valueOf(bombero.getFechaNac()));
+            ps.setString(4, bombero.getGrupoSangineo());
+            ps.setString(5, bombero.getCelular());
+            ps.setInt(6, bombero.getBrigada().getcodBrigada());// codBrigada
+            ps.setInt(7,bombero.getIdBombero());
+            int exito = ps.executeUpdate();
+            
+            if(exito == 1){
+                JOptionPane.showMessageDialog(null, "Modificado exitosamente.");
+            }else{
+                JOptionPane.showMessageDialog(null,"El bombero no existe");
+            }
+        }catch(SQLException ex){
+            JOptionPane.showMessageDialog(null,"Error al acceder a la tabla bombero "+ex.getMessage());
+        }
+    }
+    public void eliminarBombero(int id){
+        try{
+            String sql = "DELETE FROM bombero WHERE id_bombero = ?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+            int fila = ps.executeUpdate();
+            
+            if(fila==1){
+                JOptionPane.showMessageDialog(null," Se eliminó el bombero.");
+            }
+            ps.close();
+        } catch (SQLException e){
+            JOptionPane.showMessageDialog(null,"Error al acceder a bombero.");
+        }
+    }
 }
